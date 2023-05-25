55e0cc04b466bb90ceb3c5362740e208e2587c84
josiahx <jnyangaga@multiply.cloud>
2023-05-25 07:50:34 +0400

    Add gradescope autograding code

--- test_main.py
+++ test_main.py
@@ -1,12 +1,16 @@
 import unittest
 from main import *
 
+from gradescope_utils.autograder_utils.decorators import weight
+
 class Test(unittest.TestCase):
+    @weight(10)
     def test_one_by_one(self):
         matrix = [[46]]
         result = row_sum(matrix)
         self.assertEqual(result, [46])
 
+    @weight(10)
     def test_three_by_three(self):
         matrix = [
             [10, 10, 10],
@@ -16,6 +20,7 @@ class Test(unittest.TestCase):
         result = row_sum(matrix)
         self.assertEqual(result, [30, 96, 75])
 
+    @weight(10)
     def test_six_by_three(self):
         matrix = [
             [10, 10, 10, 15, 25, 35],
@@ -25,6 +30,7 @@ class Test(unittest.TestCase):
         result = row_sum(matrix)
         self.assertEqual(result, [105, 192, 171])
 
+    @weight(10)
     def test_awkward_six_rows(self):
         matrix = [
             [10, 10, 10],
